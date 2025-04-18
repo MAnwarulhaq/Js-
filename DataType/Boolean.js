a7d58670c110603901d4267e8ea5669d496150ab
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-04-18 17:01:50 +0500

    Boolean in js

---
+++ DataType/Boolean.js
@@ -0,0 +1,10 @@
+//What is a Boolean?
+// A Boolean is a data type that can only have two values: true or false. It's often used in conditional statements and logical operations.
+let isLoggedIn = true;
+let hasAccess = false;
+
+// Comparisons return booleans:
+console.log(5 > 3);     // true
+console.log(10 === 10); // true
+console.log("a" !== "b"); // true
+console.log(7 <= 6);    // false
