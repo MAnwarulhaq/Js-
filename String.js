a0afff4170d9956011042be9549039e25df213cb
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-04-18 16:50:08 +0500

    Numbers i Js

---
+++ String.js
@@ -0,0 +1,15 @@
+let name = 'Haadi';
+let greeting = "What's up?";
+let fancy = `Hello, ${name}!`; // template literal with variable
+// What is a string in JavaScript?
+//A string is just a sequence of characters used to represent text. You can write strings in:
+
+//Single quotes: 'hello'
+
+//Double quotes: "hello"
+
+//Backticks (template literals): `hello`
+
+console.log(name); // Output: Haadi
+console.log(greeting); // Output: What's up?
+console.log(fancy); // Output: Hello, Haadi!
